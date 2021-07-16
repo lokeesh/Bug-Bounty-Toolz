fed4756591008ba7b60803796d6f7820ec76e611
lokeesh <44634770+lokeesh@users.noreply.github.com>
2021-07-16 07:35:36 -0400

    Update phone-location.py
    use in india

--- phone-location.py
+++ phone-location.py
@@ -7,7 +7,7 @@ import sys
 if len(sys.argv) == 1:
 	print('Usage:')
 	print('\t%s +[country code]number'%(sys.argv[0]))
-	print('\tExample: %s +390000000000\n'%(sys.argv[0]))
+	print('\tExample: %s +91000000000\n'%(sys.argv[0]))
 	sys.exit(0)
 
 content = requests.get('https://www.phone-location.info/socialcheck/phone?provider=phone&path=%s'%(sys.argv[1])).content
